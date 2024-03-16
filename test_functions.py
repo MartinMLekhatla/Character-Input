c5f90b77e9e5a2826a8819a9590a28138cd465cd
MartinMLekhatla <martinlekhatla@gmail.com>
2024-03-16 20:44:56 +0200

    Added a function that accepts user's age

--- test_functions.py
+++ test_functions.py
@@ -9,6 +9,22 @@ class MyTestCase(unittest.TestCase):
     def test_get_user_name(self, _):
         self.assertEqual(functions.accept_users_name(), "Martin")
 
+    @patch('builtins.input', return_value='25')
+    def test_get_user_age_valid_input_type(self, _):
+        self.assertIsInstance(functions.accept_users_age(), int)
+
+    @patch('builtins.input', return_value='')
+    def test_get_user_age_empty_input(self, _):
+        self.assertIsNone(functions.accept_users_age())
+
+    @patch('builtins.input', return_value='25')
+    def test_get_user_age_valid_input(self, _):
+        self.assertEqual(functions.accept_users_age(), 25)
+
+    @patch('builtins.input', return_value='not_an_age')
+    def test_get_user_age_invalid_input(self, _):
+        self.assertIsNone(functions.accept_users_age())
+
 
 if __name__ == '__main__':
     unittest.main()
